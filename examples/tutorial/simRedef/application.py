3964f0486b9583f3be97e22b165a4116add07ab0
Sebastien Martinez <sebastien.martinez@telecom-bretagne.eu>
2014-12-11 14:11:25 +0100

    Adding tutorial example : simRedef

---
+++ examples/tutorial/simRedef/application.py
@@ -0,0 +1,45 @@
+#/usr/bin/pypy-dsu
+from pymoult.highlevel.managers import SafeRedefineManager
+from pymoult.highlevel.listener import Listener
+import time
+import threading
+import random
+
+shared = None
+shared_lock = threading.Lock()
+
+def read_shared():
+    shared_lock.acquire()
+    time.sleep(1)
+    print("Value of shared : "+str(shared))
+    shared_lock.release()
+
+
+def write_shared():
+    global shared
+    shared_lock.acquire()
+    time.sleep(1)
+    shared = random.randint(0,10)
+    shared_lock.release()
+
+def reader_f():
+    while True:
+        time.sleep(0.3)
+        read_shared()
+
+def writer_f():
+    while True:
+        time.sleep(0.5)
+        write_shared()
+        
+
+writer = threading.Thread(target=writer_f)
+writer.start()
+reader = threading.Thread(target=reader_f)
+reader.start()
+
+manager = SafeRedefineManager([reader,writer],sleepTime=0.5)
+manager.start()
+
+listener = Listener()
+listener.start()
