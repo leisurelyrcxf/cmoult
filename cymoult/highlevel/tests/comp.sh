4142d43779df45caffe7040019725e82501621be
Sebastien Martinez <sebastien.martinez@telecom-bretagne.eu>
2015-03-04 15:58:58 +0100

    Threaded and Non-Threaded Manager
    - No soultion yet for pausing threads
    - No elegent way to request a manager

---
+++ cymoult/highlevel/tests/comp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#gcc -std=c11 -g -O0 -I ../../includes -rdynamic -pthread -o threaded_listener threaded_listener.c -ldl 
+#gcc -std=c11 -g -O0 -I ../../includes -rdynamic -pthread -o test test.c threaded_listener.c common.c -ldl 
+
+gcc -std=c11 -g -O0 -I ../includes -pthread -fPIC -c ../src/managers/threaded_manager.c ../src/managers/common.c ../src/managers/manager.c update.c 
+gcc -std=c11 -g -O0 -shared -Wl,-soname,update.so.1 -o update update.o threaded_manager.o common.o manager.o
+
+gcc -std=c11 -g -O0 -I ../includes -rdynamic -pthread -o test test.c ../src/listeners/threaded_listener.c ../src/listeners/common.c ../src/managers/threaded_manager.c ../src/managers/common.c ../src/managers/manager.c -ldl
+
+
+
