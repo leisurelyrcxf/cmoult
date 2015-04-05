a28db9eda9f1a6b2e078954f5887d7133354fa5a
dgilly <dgilly@localhost>
2015-04-05 03:35:01 +0200

    Added most of tetris functions - Still in Progress

--- cymoult/demos/snake2tetris/snake.c
+++ cymoult/demos/snake2tetris/snake.c
@@ -6,25 +6,56 @@
 gameConfig gc;
 gameData gd;
 
-int main(){
-        //Start Game
-        initGameConfig(&gc);
-        initGameData(&gd,&gc);
-        clearConsole();
-        drawBoard(&gd);
-        drawGamePanel(&gc,&gd);
-        while(1){
-                delayTurn(&gc);
-                getDirection(&gd);
-                moveSnake(&gc,&gd);
+
+int main(int argc, char **argv){
+        if(argc>1){
+                if(strcmp(argv[1],"tetris")==0){
+                //Start Game
+                initGameConfig(&gc);
+                initGameDataTetris(&gd,&gc);
+                initTetrisForms(&gc);
+                generateRandomForm(&gc,&gd);
+                updateMapTetris(&gd);
                 clearConsole();
-                if(gd.lost == 0){
-                        drawBoard(&gd);
-                        drawGamePanel(&gc,&gd);
+                drawBoard(&gd);
+                drawGamePanel(&gc,&gd);
+                while(1){
+                        delayTurn(&gc);
+                        getDirectionTetris(&gc,&gd);
+                        moveTetris(&gc,&gd);
+                        clearConsole();
+                        if(gd.lost == 0){
+                                drawBoard(&gd);
+                                drawGamePanel(&gc,&gd);
+                        }
+                        else{
+                                drawLost(&gd);
+                                exit(EXIT_SUCCESS);
+                        }
                 }
-                else{
-                        drawLost(&gd);
-                        exit(EXIT_SUCCESS);
+
+                }
+        }else{
+                //Start Game
+                initGameConfig(&gc);
+                initGameData(&gd,&gc);
+                initTetrisForms(&gc);
+                clearConsole();
+                drawBoard(&gd);
+                drawGamePanel(&gc,&gd);
+                while(1){
+                        delayTurn(&gc);
+                        getDirection(&gc,&gd);
+                        moveSnake(&gc,&gd);
+                        clearConsole();
+                        if(gd.lost == 0){
+                                drawBoard(&gd);
+                                drawGamePanel(&gc,&gd);
+                        }
+                        else{
+                                drawLost(&gd);
+                                exit(EXIT_SUCCESS);
+                        }
                 }
         }
         return 0;
