d565b60f970213d2a0ac44f6e1bccbfec99e42b2
dgilly <dgilly@localhost>
2015-04-10 15:53:19 +0200

    Bug fix Snake 2 Tetris - Demo Working

--- cymoult/demos/snake2tetris/functions.c
+++ cymoult/demos/snake2tetris/functions.c
@@ -3,7 +3,7 @@
 void initGameConfig(gameConfig* gc){
         gc->height=15;
         gc->width=11;
-        gc->timeDelay=10000;
+        gc->timeDelay=2000;
 }
 
 void initGameData(gameData* gd,gameConfig* gc){
@@ -292,14 +292,18 @@ void moveTetris(gameConfig* gc, gameData* gd){
                 /* Down snake */
                 for(int i=0;i<gc->width;i++){
                         int j=(gc->height)-1;
+                        int count=0;
                         while(j>0){
-                                if(gd->map[j][i]==' '){
+                                if(gd->map[j][i]==' ' && count<j){
                                         for(int k=j;k>0;k--){
                                                 gd->map[k][i]=gd->map[k-1][i];
                                         }
+                                        count++;
                                 }
-                                else
+                                else{
+                                        count=0;
                                         j--;
+                                }
                         }
                 }
                 generateRandomForm(gc,gd);
