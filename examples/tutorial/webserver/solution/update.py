0351176ced1d01bd7bb2acf2a8012966e88a569e
Alexandre Manoury <alexandre.manoury@telecom-bretagne.eu>
2015-04-17 14:08:40 +0200

    [fix] herited class update and thread resuming

---
+++ examples/tutorial/webserver/solution/update.py
@@ -0,0 +1,259 @@
+#parsed
+from pymoult.highlevel.updates import EagerConversionUpdate,LazyConversionUpdate,Update
+from pymoult.lowlevel.data_access import DataAccessor
+from pymoult.lowlevel.alterability import waitStaticPoints
+from pymoult.lowlevel.data_update import updateToClass,addFieldToClass
+from pymoult.lowlevel.stack import resumeThread
+import sys
+
+from Cookie import SimpleCookie
+
+
+main = sys.modules["__main__"]
+
+#Step one : Updating the pages and the sessions
+
+#Updating the Page class so it tells the login in use
+class PageV2(object):
+    def __init__(self,path,title,content):
+        self.path = path
+        self.title = title
+        self.content = content
+  
+    def call(self,session):
+        self.n = int(session.values[self.path]) + 1
+        session.values[self.path] = str(self.n)
+        self.name = session.login
+
+    def __str__(self):
+       s = "<html><head><title>"+self.title+"</title></head>"
+       s+= "<body><h1>"+self.title+"</h1>"
+       s+= "<p>"+self.content+"</p>"
+       s+= "<p>"+self.name+", you saw this page "+str(self.n)+" times</p></body></html>"
+       return s
+
+#Class Session with login support
+class SessionV2(object):
+    session_id = main.Session.session_id
+    def __init__(self,items,login,session_id=None):
+        self.login = login
+        self.values = items
+        if session_id and session_id <= SessionV2.session_id:
+            self.session_id = session_id
+        else:
+            self.session_id = SessionV2.session_id
+            SessionV2.session_id += 1
+
+    def get_id(self):
+        return self.session_id
+
+    def cookie(self):
+        c = SimpleCookie()
+        for key in self.values.keys():
+            c[key] = self.values[key]
+        c["session_id"] = self.session_id
+        return c
+
+#Using Lazy conversion for sessions
+
+#Create a transformer
+#Each session opened before the update will be considered as "anonymous" session
+def session_trans(obj):
+    obj.login = "anonymous"
+
+#Start the conversion with the Update class
+sessionUpd = LazyConversionUpdate(main.Session,SessionV2,session_trans)
+main.manager.add_update(sessionUpd)
+
+#Using eager update to update pages
+
+#We need to enable the eager manager when starting the application !!!
+
+pageUpd = EagerConversionUpdate(main.Page,PageV2,None)
+main.manager.add_update(pageUpd)
+
+
+#Step two : updating the webserver
+#We will proceed in 2 substeps : (1) modify its state and (2) modify its methods
+
+#Substep 1 : modifying the state of the webserver
+
+class User(object):
+    def __init__(self,login,passwd):
+        self.login = login
+        self.passwd = passwd
+
+    def log_in(self,passwd):
+        return passwd == self.passwd
+
+class StaticPage(main.Page):
+    def __init__(self,path,title,content):
+        self.path = path
+        self.title = title
+        self.content = content
+
+    def call(self,session):
+        pass
+
+    def __str__(self):
+        s = "<html><head><title>"+self.title+"</title></head>"
+        s+= "<body><h1>"+self.title+"</h1>"
+        s+= "<p>"+self.content+"</p>"
+        return s
+
+loginPage = StaticPage("login","You are not logged in!","""
+<form method="get" action="logged">
+Login : <input type="text" name="login">
+Password: <input type="password" name="password">
+<input type="submit" value="Log in">
+</form>
+""")
+
+loggedPage = StaticPage("logged","Logged in!","You are logged in!")
+notLogged = StaticPage("nlogged","Login failed","You are not logged in")
+
+
+#First we need to retrieve the webserver, for this purpose we will use
+#the DataAccessor from the lowlevel API
+
+#Remember : using the immediate strategy requires the ObjectPool to be
+#created before creating the webserver !!
+
+class WebServerStateUpdate(Update):
+    def requirements(self):
+        return True
+    def alterability(self):
+        return True
+    def apply(self):
+        da = DataAccessor(main.WebServer,strategy="immediate")
+        #Because we'll need it for the next update,
+        #we store the server in the manager
+        self.manager.server = None
+        for ws in da:
+            self.manager.server = ws
+        #Then, we add the static pages to the webserver
+        self.manager.server.pages['login'] = loginPage
+        self.manager.server.pages['logged'] = loggedPage
+        self.manager.server.pages['nlogged'] = notLogged
+        #We add a users attribute to the server, containing the users
+        #We add a new user "bob" with password "alice"
+        self.manager.server.users = {"bob":User("bob","alice")}
+    def over(self):
+        return True
+
+serverUpdate1 = WebServerStateUpdate()
+main.manager.add_update(serverUpdate1)
+
+#Substep 2 :  modifiy the methods of the webserver
+
+class WebServerV2(object):
+    def __init__(self,pages):
+        self.pages = {}
+        for page in pages:
+            self.pages[page.path] = page
+        #Generate a Handler class specifically for the current Webserver.
+        self.Handler = produceHandler(self)
+        self.sessions = {}
+        self.users = {}
+
+    def addSession(self,cookie=None):
+        cdict = {}
+        session_id = None
+        login="anonymous"
+        for pageK in self.pages.keys():
+            cdict[pageK] = 0
+        if cookie:
+            for key in cookie.keys():
+                if key == "session_id":
+                    session_id =int(cookie[key].value)
+                elif key == "login":
+                    login = cookie[key].value
+                else:
+                    cdict[key] = cookie[key].value
+        s = SessionV2(cdict,login,session_id)
+        self.sessions[str(s.get_id())] = s
+        return s
+
+    def run(self):
+        #Create a http server listening on localhost port 8080
+        #The server will use the self.Handler class to handle the clients requests
+        httpd = HTTPServer(("",8080),self.Handler)
+        while True:
+            staticUpdatePoint()
+            httpd.handle_request()
+    
+
+def create_new_do_GET(webserver):
+    def new_do_GET(handler):
+        path = handler.path.lstrip("/")
+        session = handler.getSession()
+        if path.startswith("logged"):
+            l = path[7:].split("&")
+            login = l[0][6:]
+            passwd = l[1][9:]
+            if login in webserver.users.keys():
+                user = webserver.users[login]
+                if user.log_in(passwd):
+                    path="logged"
+                    session.login = login
+                else:
+                    path="nlogged"
+            else:
+                path = "nlogged"
+            page = webserver.pages[path]
+            handler.send_response(200)
+            handler.send_header("Content-type","text/html")
+            handler.end_headers()
+            handler.wfile.write(page)
+        elif path in webserver.pages.keys():
+            if session.login == "anonymous":
+                page = webserver.pages["login"]
+            else:
+                page = webserver.pages[path]
+            page.call(session)
+            #Response OK
+            handler.send_response(200)
+            #Headers including the cookie
+            handler.send_header("Content-type","text/html")
+            handler.send_header("Set-Cookie", session.cookie().output(header=""))
+            handler.end_headers()
+            handler.wfile.write(page)
+        else:
+            handler.send_response(404)
+            handler.send_error(404, "Page not found")
+    return new_do_GET
+
+
+# First, we need to consider the alterability of the application.  We
+# will place a staticUpdatePoint in the "run" method of the webserver,
+# at the begining of the while loop.
+
+#We will wait for the main_thread to reach that update point
+
+class WebServerUpdate(Update):
+    def requirements(self):
+        return True
+    def alterability(self):
+        #The static point has been reached !!!
+        #Let's continue the update
+        waitStaticPoints([main.main_thread])
+        return True
+    def apply(self):
+        #To update the addSession method of the webserver, we need to update the WebServer class
+        updateToClass(self.manager.server,WebServerV2)
+        #We need to change the do_GET method of the Handler class possesed by the server
+        handler = self.manager.server.Handler
+        #We create a new do_GET method using create_new_do_GET and set it to the handler class
+        addFieldToClass(handler,"do_GET",create_new_do_GET(self.manager.server))
+
+    def over(self):
+        #We have finished the update, we need to resume the execution of the Thread
+        resumeThread(main.main_thread)
+        #Let's tell we finished the update
+        print("UPDATE COMPLETE")
+        return True
+
+
+webUpdate = WebServerUpdate()
+main.manager.add_update(webUpdate)
+
---
+++ pymoult/highlevel/updates.py
@@ -0,0 +1,174 @@
+#    updates.py This file is part of Pymoult
+#    Copyright (C) 2013 Sébastien Martinez, Fabien Dagnat, Jérémy Buisson
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+"""pymoult.updates.py
+   Published under the GPLv2 license (see LICENSE.txt)
+
+   Provides the Update classes to be used with the managers in the
+   managers module.
+
+"""
+
+from pymoult.lowlevel.stack import *
+from pymoult.lowlevel.relinking import *
+from pymoult.lowlevel.alterability import *
+from pymoult.lowlevel.data_access import *
+from pymoult.lowlevel.data_update import *
+from pymoult.threads import *
+from pymoult.highlevel.listener import get_app_listener
+
+
+def isApplied(update):
+    listener = get_app_listener()
+    return update in listener.applied_updates
+
+class UpdateDefinitionError(Exception):
+    """Exception raised when using an Update interface"""
+    def __init__(self,message):
+        super(UpdateDefinitionError,self).__init__()
+        self.message = message
+
+
+class Update(object):
+    def __init__(self,name=None):
+        self.applied = False
+        self.manager = None
+        self.name = name
+
+    def requirements(self):
+        raise UpdateDefinitionError("You should define your own Update-based class")
+        
+    def alterability(self):
+        raise UpdateDefinitionError("You should define your own Update-based class")
+
+    def apply(self):
+        raise UpdateDefinitionError("You should define your own Update-based class")
+    
+    def over(self):
+        raise UpdateDefinitionError("You should define your own Update-based class")
+
+    
+class SafeRedefineUpdate(Update):
+    def __init__(self,module,function,new_function,name=None):
+        self.module = module
+        self.function = function
+        self.new_function = new_function
+        super(SafeRedefineUpdate,self).__init__(name=name)
+
+    def requirements(self):
+        return True
+        
+    def alterability(self):
+        return not isFunctionInAnyStack(self.function,threads=self.manager.threads)
+        
+
+    def apply(self):
+        redefineFunction(self.module,self.function,self.new_function)
+
+    def over(self):
+        return True
+    
+class EagerConversionUpdate(Update):
+    def __init__(self,cls,new_cls,transformer,name=None):
+        self.cls = cls
+        self.new_cls = new_cls
+        self.transformer = transformer
+        super(EagerConversionUpdate,self).__init__(name=name)
+
+    def object_update(self,obj):
+        updateToClass(obj,self.new_cls,self.transformer,self.cls)
+        
+    def requirements(self):
+        return True
+
+    def alterability(self):
+        return True
+
+    def apply(self):
+        startEagerUpdate(self.cls,self.object_update)
+
+    def over(self):
+        return True
+
+
+class LazyConversionUpdate(Update):
+    def __init__(self,cls,new_cls,transformer,name=None):
+        self.cls=cls
+        self.new_cls=new_cls
+        self.transformer = transformer
+        super(LazyConversionUpdate,self).__init__(name=name)
+
+    def object_update(self,obj):
+        updateToClass(obj,self.new_cls,self.transformer,self.cls)
+
+    def requirements(self):
+        return True
+
+    def alterability(self):
+        return True
+
+    def apply(self):
+        setLazyUpdate(self.cls,self.object_update)
+
+    def over(self):
+        return True
+        
+
+class ThreadRebootUpdate(Update):
+    def __init__(self,thread,new_main,new_args,name=None):
+
+        if thread.__class__ is not DSU_Thread and not DSU_Thread in thread.__class__.__bases__:
+            raise TypeError("threads in ThreadRebootManager must be of type or of a subtype of DSU_Thread")
+        self.thread = thread
+        self.new_main = new_main
+        self.new_args = new_args
+        super(ThreadRebootUpdate,self).__init__(name=name)
+
+    def requirements(self):
+        return True
+
+    def alterability(self):
+        return True
+    
+    def apply(self):
+        resumeThread(self.thread)
+        switchMain(self.thread,self.new_main,args=self.new_args)
+        resetThread(self.thread)
+
+    def over(self):
+        return True
+
+
+class HeapTraversalUpdate(Update):
+    def __init__(self,walker,modules=["__main__"],name=None):
+        self.walker = walker
+        self.modules = modules
+        super(HeapTraversalUpdate,self).__init__(name=name)
+
+    def requirements(self):
+        return True
+
+    def alterability(self):
+        return True
+
+    def apply(self):
+        traverseHeap(self.walker,self.modules)
+        
+    def over(self):
+        return True
+    
