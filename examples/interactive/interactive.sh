1b8766147bd237a7026d72ad6a7a05fa394f23ee
Sebastien Martinez <sebastien.martinez@telecom-bretagne.eu>
2015-05-05 18:37:35 +0200

    Update + interactive senario until update

---
+++ examples/interactive/interactive.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+TITLE="Chose your own policy update!"
+TPATH=/tmp/interactive
+TMP=$TPATH/ans
+MANAGER=""
+STATIC=""
+
+mkdir $TPATH
+
+INTERPATH=$(realpath $(dirname $0))
+cp $INTERPATH/application.py $TPATH/
+cp $INTERPATH/update.py $TPATH/
+
+dialog --msgbox "$TITLE\n\nHere, you will chose how to update your file server!\n\nLet's take a look at the code!" 10 80 
+
+dialog --textbox $TPATH/application.py 120 150
+
+dialog --msgbox "$TITLE\n\nBefore starting the application, we need to decide a few things ..." 10 80
+
+dialog --menu "$TITLE\n\nDo we add a manager to the application?" 30 80 3 "t" "add a threaded manager" "m" "add a non-threaded manager" "n" "do not add a manager" 2>$TMP
+
+MANAGER=$(cat $TMP)
+
+if [ "$MANAGER" == "t" ]
+then
+    #We decided to add a global threaded manager
+    sed -i 's/import os/import os\nfrom pymoult.highlevel.managers import ThreadedManager/' $TPATH/application.py
+    sed -i 's/listener.start()/listener.start()\n    manager = ThreadedManager(name="pymoult",threads=[])\n    manager.start()/' $TPATH/application.py
+
+    dialog --textbox $TPATH/application.py 120 150
+    
+elif [ "$MANAGER" == "m" ]
+then
+    #We decided to add a non threaded manager
+    sed -i 's/import os/import os\nfrom pymoult.highlevel.managers import Manager/' $TPATH/application.py
+    sed -i 's/files = {}/manager = Manager(name="pymoult")\nfiles = {}/' $TPATH/application.py 
+
+    #Decide where to make the manager run 
+
+    dialog --menu "$TITLE\n\nWhere do we run the manager?" 30 80 2 "m" "in main's loop" "t" "in threads loop" 2>$TMP
+    STATIC=$(cat $TMP)
+   
+    if [ "$STATIC" == "m" ]
+    then
+        sed -i 's/while True:/while True:\n        manager.apply_next_update()/' $TPATH/application.py
+    else
+        sed -i 's/while self.connection:/while self.connection:\n            manager.apply_next_update()/' $TPATH/application.py    
+    fi
+
+fi
+
+if [ "$MANAGER" != "m" ]
+then
+    dialog --menu "$TITLE\n\nDo we put static update points?" 30 80 3 "m" "put a static point in main's loop" "t" "put a static point in threads loop" "n" "do not put static points" 2>$TMP
+    
+   STATIC=$(cat $TMP)
+   
+   if [ "$STATIC" == "m" ]
+   then
+       sed -i 's/import os/import os\nfrom pymoult.lowlevel.alterability import staticUpdatePoint\nfrom pymoult.threads import DSU_Thread/' $TPATH/application.py
+       sed -i 's/  main()/  main_thread = DSU_Thread(target=main)\n    main_thread.start()/' $TPATH/application.py
+       #place the point
+       sed -i 's/while True:/while True:\n        staticUpdatePoint()/' $TPATH/application.py
+       
+   elif [ "$STATIC" == "t" ]
+   then
+        sed -i 's/import os/import os\nfrom pymoult.lowlevel.alterability import staticUpdatePoint\nfrom pymoult.threads import DSU_Thread/' $TPATH/application.py
+        sed -i 's/threading.Thread/DSU_Thread/' $TPATH/application.py
+        sed -i 's/run/main/' $TPATH/application.py
+        #place the point
+        sed -i 's/while self.connection:/while self.connection:\n            staticUpdatePoint()/' $TPATH/application.py    
+   fi
+   
+fi
+
+dialog --textbox $TPATH/application.py 120 150
+
+dialog --msgbox "$TITLE\n\nWe are now ready to start the application." 8 80
+
+dialog --inputbox "$TITLE\n\nPlease enter paths of the folders to serve (separated by space)" 12 80 2> $TMP
+folders=$(cat $TMP)
+
+pypy-dsu $TPATH/application.py $folders &
+app=$!
+
+dialog --msgbox "$TITLE\n\nNow, it's time to launch the client." 8 80
+
+dialog --msgbox "$TITLE\n\nReady for a Dynamic Update?\nLet's take a look at it." 10 80
+
+dialog --textbox $TPATH/update.py 120 150
+
+
+
+
+
+
+kill $app
+
+rm -r $TPATH
--- examples/interactive/update.py
+++ examples/interactive/update.py
@@ -5,19 +5,28 @@ import socket
 import sys
 import os
 import threading
-from pymoult.highlevel.listener import Listener
-
+import tempfile
 
 class Picture(object):
     def __init__(self,path,name):
+        self.basepath = path
         self.path = path
         self.name = name
+        self.commentary = "Witty comment"
 
     def stream(self):
         f = open(self.path,'r')
         stream = f.read()
         f.close()
         return stream
+
+    def comment(self,text):
+        self.commentary = text
+
+    def annotate(self):
+        new_file = os.path.join(tempfile.gettempdir(),os.path.basename(self.basepath)+"_anot.jpg")
+        self.path = new_file
+        os.system("convert "+self.basepath+" -background Plum -font Corsiva -pointsize 24 label:'"+self.commentary+"' -gravity Center -append "+self.path)
 
 files = {}
 
@@ -32,13 +41,14 @@ class ConnThread(threading.Thread):
     def __init__(self,connection):
         self.connection = connection
         self.welcome = "Welcome to the server. Here are the available folders "+" ".join(files.keys())+".\nPlease send the folder you want to download\n"
-        self.help = "help : shows this help\nexit : disconnects\n<folder name> : downloads pictures from the folder\n(Available folders : "+" ".join(files.keys())+")\n"
+        self.help = "help : shows this help\nexit : disconnects\ncomment <folder> <comment>: sets commentary for picturesof folder\n<folder name> : downloads pictures from the folder\n(Available folders : "+" ".join(files.keys())+")\n"
         threading.Thread.__init__(self)
         
     def serve_folder(self,folder):
         self.connection.sendall("serving")
         if self.connection.recv(1024).strip() == "go":
             for pic in files[folder]:
+                pic.annotate()
                 imgstream = pic.stream()
                 self.connection.sendall("<img:"+str(len(imgstream))+">"+pic.name)
                 if self.connection.recv(1024).strip() == "cancel":
@@ -55,7 +65,15 @@ class ConnThread(threading.Thread):
             self.connection.sendall("terminating")
             self.connection.close()
             self.connection = None
+        elif command[0:7] == "comment":
+            args = command.split()
+            if len(args)> 2:
+                if args[1] in files.keys():
+                    for pic in files[args[1]]:
+                        pic.comment(" ".join(args[2:]))
+            self.connection.sendall("Comment '"+" ".join(args[2:])+"' applied to folder "+args[1])
         else:
+            print(command[0:7])
             self.connection.sendall(self.help)
                                     
     def run(self):
@@ -78,8 +96,6 @@ def main():
         ConnThread(conn).start()
 
 if __name__ == "__main__":
-    listener = Listener()
-    listener.start()
     main()
         
 
