938c596ef14f4bcff2c8b5d31ee393f0d9303daa
Sebastien Martinez <sebastien.martinez@telecom-bretagne.eu>
2015-06-08 19:16:00 +0200

    Adapting cymoult to the PymoultV3 design (does not compile yet!)

--- cymoult/highlevel/includes/update.h
+++ cymoult/highlevel/includes/update.h
@@ -1,11 +1,32 @@
+#ifndef UPDATE_H
+#define UPDATE_H
+
 #include <stdlib.h>
+#include "dsuthread.h"
+
+typedef enum ra {
+  yes,
+  no,
+  never
+} req_ans;
 
 typedef struct{
-  char (*requirements)();
-  char (*alterability)();
+  char * name;
+  dsuthread * threads;
+  size_t nthreads;
+  int max_tries;
+  req_ans (*check_requirements)();
+  void (*preupdate_setup)();
+  char (*check_alterability)();
+  char (*wait_alterability)();
+  void (*clean_failed_alterability)();
   void (*apply)();
-  char (*over)();
+  void (*preresume_setup)();
+  char (*wait_over)();
+  char (*check_over)();
+  void (*cleanup)();
   char applied;
 }update;
 
 
+#endif /* End ifndef UPDATE_H */
