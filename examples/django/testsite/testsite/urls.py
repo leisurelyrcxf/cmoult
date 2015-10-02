185964736cfd7eaf3ef3882fd12d1eb8d67ed0c1
Sebastien Martinez <sebastien.martinez@telecom-bretagne.eu>
2015-10-02 18:00:12 +0200

    A simple "Hello" for the django app

--- examples/django/testsite/testsite/urls.py
+++ examples/django/testsite/testsite/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from testsite.views import Home
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'testsite.views.home', name='home'),
+    url(r'^$', Home.as_view()),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
