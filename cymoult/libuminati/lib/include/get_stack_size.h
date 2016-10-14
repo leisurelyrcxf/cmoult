894c6011cfffd28c08b92052943f65d24c674ee0
xiaofan chen <xiaofan chen@localhost>
2016-10-14 22:48:22 +0800

    remove cymoult

--- cymoult/libuminati/lib/include/get_stack_size.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_STACK_SIZE
-#define GET_STACK_SIZE
-#include "libc_includes.h"
-#include "data_structs.h"
-#include "get_next_frame.h"
-
-int um_get_stack_size (um_data* dbg, um_frame* stack);
-
-#endif
--- cymoult/libuminati/testprogs/src/update.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-void not_in_stack_v2(int* b) {
-    *b = 42;
-    //printf("a = %d\n", *b);
-}
